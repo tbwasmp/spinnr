bc09f74c1cee52938a284c41c8989aa4cb16909b
Ralph Reyes <ralph.reyes@tbwa-smp.com>
2015-05-27 13:30:37 +0800

    Revert "I DELETED THE APP FOLDER!!!"
    This reverts commit dab43845a2de8aab2f45a2de0a11cbba770e8114.

---
+++ app/components/registration/partials/registration-share.php
@@ -0,0 +1,17 @@
+<div class="tag small">
+  <div class="wrapper">
+  	<h1 class="three">Share</h1>
+  	<blockquote>
+  		<p>You picked {{ formData.artist }} and earned yourself a raffle entry by registering!</p>
+  		<p><strong>Stay tuned for more ways of earning entries to win awesome prizes!</strong></p>
+  	</blockquote>
+  	<small>*Spinnr Tip: Share this activity to earn another raffle entry!</small>
+  </div>
+</div>
+
+<div class="form-group row">
+  <div class="col-xs-6 col-xs-offset-1">
+    <a href="/" class="ok btn">OK</a>
+    <button type="button" class="share btn">Share</button>
+  </div>
+</div>
